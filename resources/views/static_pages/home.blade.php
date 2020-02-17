84966c6f0a2d358b2817fbbb75f1c40a3ffa2958
吴瑞鹏 <137584321@qq.com>
2020-02-17 03:26:26 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,13 +4,13 @@
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">
-      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">小吴</a> 倾心打造的示例项目主页。
+      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
     </p>
     <p>
-      保存项目不是开玩笑的。
+      一切，将从这里开始。
     </p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
     </p>
   </div>
 @stop
